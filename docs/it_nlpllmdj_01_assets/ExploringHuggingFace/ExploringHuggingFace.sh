5a70d7d071cfebdddbd5a3fd8ce9d5c0bc251676
donadviser <donadviser@gmail.com>
2025-02-13 10:49:19 +0000

    docs and transcript

---
+++ docs/it_nlpllmdj_01_assets/ExploringHuggingFace/ExploringHuggingFace.sh
@@ -0,0 +1,106 @@
+------------------------------------------------------------------------
+demo-00-ExploringHuggingFace
+------------------------------------------------------------------------
+
+# Go to https://huggingface.co/
+
+# Scroll and show
+
+# Sign up for a new account
+
+# Will need to verify from email after signing up
+
+# Dismiss the message you see on top
+
+# Click on Models - scroll and show
+
+# Show the NLP tasks on the left
+
+# Click on
+
+Text Classification
+
+# Select this model and show the model card
+
+ProsusAI/finbert
+
+# Click on the tabs
+
+Files and versions
+Community
+
+# Use the inference API on the right in the Model Card with these examples
+
+The prospect of a Fed rate cut had a galvanizing effect on the markets
+
+Overall liquidity was tight and markets reflected that
+
+Stocks were bouyant on positive economic news but worries remain
+
+
+# Go back to the main Models page (can click on the tab) and choose another NLP task
+
+Summarization
+
+# Click on this model and show the card
+
+facebook/bart-large-cnn
+
+-------------------------------------------
+
+# Click on Datasets on top - scroll and show the page
+
+# Under NLP select this task
+
+Question Answering
+
+# Scroll a bit and at the bottom right click on
+
+squad
+
+# Show that it has training and validation data
+
+# Scroll and show
+
+# Back to Datasets
+
+# Click on 
+
+Text Classification
+
+
+# Here click on the dataset
+
+imdb
+
+# Scroll and show
+
+
+-------------------------------------------
+
+# Click on Spaces
+
+# SCroll and shwo
+
+# Click on 
+
+Screenshot2HTML
+
+# Upload the screenshot from the screenshots/ folder
+
+HF_screenshot
+
+# Show the rendered HTML (a little slow)
+
+
+
+
+
+
+
+
+
+
+
+
+
